349e93a2c6c83242049f52a5d144f344b47bf6fb
Tibor <Tibor.Bodnar@ibm.com>
2020-10-19 08:51:52 +0200

    Material: add elastic collision

--- material.cpp
+++ material.cpp
@@ -4,7 +4,8 @@ Material::Material(double mass,
                    double pos_x, double pos_y,
                    double velocity_x, double velocity_y,
                    double accelerate_x, double accelerate_y,
-                   int border_x_min, int border_x_max, int border_y_min, int border_y_max)
+                   int border_x_min, int border_x_max, int border_y_min, int border_y_max,
+                   double radius)
 {
   this->mass = mass;
 
@@ -21,12 +22,34 @@ Material::Material(double mass,
   this->border_x_max = border_x_max;
   this->border_y_min = border_y_min;
   this->border_y_max = border_y_max;
+
+  this->radius = radius;
 }
 
 void Material::setAccelerate(double accelerate_x, double accelerate_y)
 {
   this->accelerate_x = accelerate_x;
   this->accelerate_y = accelerate_y;
+}
+
+void Material::setPosition(double pos_x, double pos_y)
+{
+  this->pos_x = pos_x;
+  this->pos_y = pos_y;
+}
+
+void Material::elasticCollision(Material *other)
+{
+  double n_v_x, n_v_y;
+
+  n_v_x = ((this->mass - other->mass)/(this->mass + other->mass)) * this->velocity_x + ((2 * other->mass)/(this->mass + other->mass)) * other->velocity_x;
+  n_v_y = ((this->mass - other->mass)/(this->mass + other->mass)) * this->velocity_y + ((2 * other->mass)/(this->mass + other->mass)) * other->velocity_y;
+
+  other->velocity_x = ((other->mass - this->mass)/(other->mass + this->mass)) * other->velocity_x + ((2 * this->mass)/(other->mass + this->mass)) * this->velocity_x;
+  other->velocity_y = ((other->mass - this->mass)/(other->mass + this->mass)) * other->velocity_y + ((2 * this->mass)/(other->mass + this->mass)) * this->velocity_y;
+
+  this->velocity_x = n_v_x;
+  this->velocity_y = n_v_y;
 }
 
 void Material::reboundFromBorder()
@@ -55,7 +78,7 @@ void Material::reboundFromBorder()
   }
 }
 
-void Material::move(const int step)
+void Material::move(const double step)
 {
   pos_x += step * velocity_x;
   pos_y += step * velocity_y;
@@ -70,4 +93,9 @@ void Material::getPosition(double &pos_x, double &pos_y)
 {
   pos_x = this->pos_x;
   pos_y = this->pos_y;
+}
+
+void Material::getRadius(double &r)
+{
+  r = this->radius;
 }
--- material.h
+++ material.h
@@ -7,6 +7,7 @@ class Material
   double velocity_x, velocity_y;
   double accelerate_x, accelerate_y;
   double mass;
+  double radius;
 
   int border_x_min, border_x_max, border_y_min, border_y_max;
 
@@ -17,13 +18,18 @@ class Material
              double pos_x, double pos_y,
              double velocity_x = 0, double velocity_y = 0,
              double accelerate_x = 0, double accelerate_y = 0,
-             int border_x_min = 0, int border_x_max = 800, int border_y_min = 0, int border_y_max = 600);
+             int border_x_min = 0, int border_x_max = 800, int border_y_min = 0, int border_y_max = 600,
+             double radius = 1.0);
 
     void setAccelerate(double accelerate_x, double accelerate_y);
+    void setPosition(double pos_x, double pos_y);
 
-    void move(const int step);
+    void move(const double step);
 
     void getPosition(double &pos_x, double &pos_y);
+    void getRadius(double &r);
+
+    void elasticCollision(Material *other);
 };
 
 #endif // MATERIAL_H
